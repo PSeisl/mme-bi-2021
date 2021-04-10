ecf85caec8d37b7fc01c90937f4401f91acbb399
philippseisl <p.seisl95@gmail.com>
2021-04-10 11:26:32 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # MME2: BII assignment 3 - k-means clustering
 # SEISL Philipp (me20m003) & RIRSCH Karl-Philipp (me20m002)
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+import xlrd
+import pandas as pd
 
+excel_werte = pd.read_csv("input.csv", sep=";") #, skiprows=2
+#excel_werte = np.genfromtxt('input.csv', delimiter=';')
+
+
+print(excel_werte)
