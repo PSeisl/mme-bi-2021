cbccbb8de42362c1082ee76b11e9d830b23ecd5f
philipprirsch <karlphilipp.rirsch@gmail.com>
2021-04-10 16:36:30 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # MME2: BII assignment 3 - k-means clustering
 # SEISL Philipp (me20m003) & RIRSCH Karl-Philipp (me20m002)
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 import pandas as pd
+from copy import deepcopy
 
 # Read information like clusters, rows and columns
 info = np.genfromtxt('input.csv', dtype=None, encoding='utf-8-sig', delimiter=';')
@@ -30,9 +30,34 @@ std = np.std(data, axis=0)
 print(mean, std)
 centers = np.random.randn(clusters, cols)*std + mean
 
+
+
+# Iterate Centroids
+centers_old = np.zeros(centers.shape)  # to store old centers
+centers_new = deepcopy(centers)  # Store new centers
+
+data.shape
+clusters0 = np.zeros(rows)
+distances = np.zeros((rows, clusters))
+
+error = np.linalg.norm(centers_new - centers_old)
+
+# When, after an update, the estimate of that center stays the same, exit loop
+while error != 0:
+    # Measure the distance to every center
+    for i in range(clusters):
+        distances[:, i] = np.linalg.norm(data - centers_new[i], axis=1)
+    # Assign all training data to closest center
+    clusters0 = np.argmin(distances, axis=1)
+
+    centers_old = deepcopy(centers_new)
+    # Calculate mean for every cluster and update the center
+    for i in range(clusters):
+        centers_new[i] = np.mean(data[clusters0 == i], axis=0)
+    error = np.linalg.norm(centers_new - centers_old)
+centers = deepcopy(centers_new)
+
 # Create a Scatter-Plot
-print(cluster)
-x_data = data[:, 0]
-y_data = data[:, 1]
-plt.scatter(x_data, y_data, c=cluster, cmap='rainbow')
+plt.scatter(data[:, 0], data[:, 1], c=cluster, cmap='rainbow', s=10)
+plt.scatter(centers[:, 0], centers[:, 1], marker='d', c='grey', s=50)
 plt.show()
